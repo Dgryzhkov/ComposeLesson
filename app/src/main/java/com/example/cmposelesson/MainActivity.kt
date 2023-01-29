9c5bcb257d5d413d1c629422816320e16411bf8d
Denis Ryzhkov <dgryzhkov@gmail.com>
2023-01-29 23:03:29 +0700

    Card Box Image

--- app/src/main/java/com/example/cmposelesson/MainActivity.kt
+++ app/src/main/java/com/example/cmposelesson/MainActivity.kt
@@ -3,50 +3,79 @@ package com.example.cmposelesson
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(  modifier = Modifier
-                .background(Color.Blue).fillMaxHeight(0.5f).fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            Alignment.CenterVertically) {
+            Column() {
+                ListItem(name = "AAA aaaa", prof = "BBBBBBB")
+                ListItem(name = "AAA aaaa", prof = "BBBBBBB")
+                ListItem(name = "AAA aaaa", prof = "BBBBBBB")
+                ListItem(name = "AAA aaaa", prof = "BBBBBBB")
+                ListItem(name = "AAA aaaa", prof = "BBBBBBB")
+                ListItem(name = "AAA aaaa", prof = "BBBBBBB")
 
-                Column(modifier = Modifier
-                    .background(Color.Red),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.Start
-                ) {
-                    Text(text = "Hello world!")
-                    Text(text = "Hello Tomsk!")
-                    Text(text = "Hello!!!!!!!!")
-                }
-                Column(modifier = Modifier
-                    .background(Color.Green).fillMaxHeight(0.5f),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.Start
 
+            }
+        }
+    }
+}
+
+@Composable
+private fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box() {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ava),
+                    contentDescription = " Image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column(
+                    modifier = Modifier
+                        .padding(start = 16.dp)
                 ) {
-                    Text(text = "Hello world!")
-                    Text(text = "Hello Tomsk!")
-                    Text(text = "Hello!!!!!!!!")
+                    Text(text = name)
+                    Text(text = prof)
+
                 }
             }
 
-
-
         }
+
+
     }
+
 }
